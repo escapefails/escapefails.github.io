fbd3d130b6758f62fa9ec6982f5ce990750406e7
Escape Fails <escapefails@gmail.com>
2014-05-19 10:41:50 +0200

    site update

---
+++ corners.js
@@ -0,0 +1,47 @@
+
+
+
+
+
+/*
+     FILE ARCHIVED ON 11:23:31 Feb 1, 2011 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 18:58:07 May 18, 2014.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+window.onload = function()
+  {
+      /*
+      The new 'validTags' setting is optional and allows
+      you to specify other HTML elements that curvyCorners
+      can attempt to round.
+
+      The value is comma separated list of html elements
+      in lowercase.
+
+      validTags: ["div", "form"]
+
+      The above example would enable curvyCorners on FORM elements.
+      */
+      settings = {
+          tl: { radius: 6 },
+          tr: { radius: 6 },
+          bl: { radius: 6 },
+          br: { radius: 6 },
+          antiAlias: true,
+          autoPad: true,
+          validTags: ["div"]
+      }
+
+      /*
+      Usage:
+
+      newCornersObj = new curvyCorners(settingsObj, classNameStr);
+      newCornersObj = new curvyCorners(settingsObj, divObj1[, divObj2[, divObj3[, . . . [, divObjN]]]]);
+      */
+      var myBoxObject = new curvyCorners(settings, "cornercurve");
+      myBoxObject.applyCornersToAll();
+  }
+
